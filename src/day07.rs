6dca0ae946c01ff366f17b5b9f4f3e4b56c3bad1
Damien Picard <picard.dam@gmail.com>
2024-12-07 15:34:38 +0100

    feat(day07): Utiliser la force pour construire le pont par concaténation

--- src/day07.rs
+++ src/day07.rs
@@ -44,9 +44,28 @@ pub fn solve_part1(input: &Puzzle) -> u64 {
         .sum()
 }
 
+fn concat(l: u64, r: u64) -> u64 {
+    l * 10u64.pow(r.ilog10() + 1) + r
+}
+
+fn valid_equation_concat(result: u64, numbers: &[u64], bfr: u64) -> bool {
+    match numbers {
+        [] => result == bfr,
+        [head, tail @ ..] => {
+            valid_equation_concat(result, tail, bfr + head)
+                || valid_equation_concat(result, tail, concat(bfr, *head))
+                || valid_equation_concat(result, tail, bfr * head)
+        }
+    }
+}
+
 #[aoc(day7, part2)]
 pub fn solve_part2(input: &Puzzle) -> u64 {
-    input.len() as u64
+    input
+        .iter()
+        .filter(|&(k, v)| valid_equation_concat(*k, v, 0))
+        .map(|(k, _v)| k)
+        .sum()
 }
 
 #[cfg(test)]
@@ -92,8 +111,25 @@ mod tests {
         assert_eq!(part1(input), 2941973819040)
     }
 
-    #[test_case(TEST => 6)]
+    #[test_case(TEST => 11387)]
     fn part2(input: &str) -> u64 {
         solve_part2(&input_generator(input))
+    }
+
+    #[test_case("190: 10 19" => true)]
+    #[test_case("3267: 81 40 27" => true)]
+    #[test_case("83: 17 5" => false)]
+    #[test_case("156: 15 6" => true)]
+    #[test_case("7290: 6 8 6 15" => true)]
+    #[test_case("161011: 16 10 13" => false)]
+    #[test_case("192: 17 8 14" => true)]
+    #[test_case("21037: 9 7 18 13" => false)]
+    #[test_case("292: 11 6 16 20" => true)]
+    fn part2_line(input: &str) -> bool {
+        let puzzle = input_generator(input);
+        puzzle
+            .first()
+            .map(|(k, v)| valid_equation_concat(*k, v, 0))
+            .unwrap_or_default()
     }
 }
